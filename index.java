ef471c3c95a746045e4373975e8045312b96c68b
purva-creates <purvabhusum@gmail.com>
2026-04-02 01:04:22 +0530

    add parameter constructor

---
+++ index.java
@@ -0,0 +1,18 @@
+package demo;
+
+public  class index{
+   
+    int time;
+    String place;
+    //constructor
+    index(int t, String p){
+        this.time=t;
+        this.place=p;
+    }
+     public static void main(String[] args){
+    index c=new index(45,"Amravati");
+
+    System.out.println(c);
+    
+}
+}
